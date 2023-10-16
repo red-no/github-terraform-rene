1eae32310b2e6885a84ed1a19d13afa6b851b2cb
René Meinecke <github@meinecke.no>
2023-10-16 20:47:03 +0200

    Øving 8 CI/CD Workspaces

--- staticwebsite/variables.tf
+++ staticwebsite/variables.tf
@@ -18,7 +18,7 @@ variable "sa_name" {
 variable "source_content" {
   type        = string
   description = "Source content for the index.html file"
-  default     = "<h1>Web page created with Terraform</h1>"
+  default     = "<h1>Web page created with Terraform - CI/CD</h1>"
 }
 
 variable "index_document" {
