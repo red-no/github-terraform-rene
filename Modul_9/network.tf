54e99485510fe5afb4398929307178ff135057e0
René Meinecke <github@meinecke.no>
2023-12-10 14:41:40 +0100

    før eksamen

--- Modul_9/network.tf
+++ Modul_9/network.tf
@@ -32,7 +32,7 @@ resource "azurerm_network_security_rule" "ssh_inbound_myIP" {
   protocol                    = "Tcp"
   source_port_range           = "22"
   destination_port_range      = "22"
-  source_address_prefix      = "178.164.126.112"
+  source_address_prefix       = "178.164.126.112"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg-infra.name
   network_security_group_name = azurerm_network_security_group.nsg.name
--- Modul_9/providers.tf
+++ Modul_9/providers.tf
@@ -1,24 +1,24 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "3.75.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.84.0"
     }
+  }
 
-        backend "azurerm" {
-        resource_group_name  = "rg-backend-tfstate-remei"               # må være kjent
-        storage_account_name = "satfstateremei9hdsbq7hez"               # må være kjent
-        container_name       = "sctfstateremei"                         # må være kjent
-        key                  = "modul9-remei.terraform.tfstate"         # må være hardcodet
-    }
+  backend "azurerm" {
+    resource_group_name  = "rg-backend-tfstate-remei"       # må være kjent
+    storage_account_name = "satfstateremei9hdsbq7hez"       # må være kjent
+    container_name       = "sctfstateremei"                 # må være kjent
+    key                  = "modul9-remei.terraform.tfstate" # må være hardcodet
+  }
 }
 
 provider "azurerm" {
-    features {
-        key_vault {
-            purge_soft_delete_on_destroy    = true
-            recover_soft_deleted_key_vaults = true
-        }
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
     }
+  }
 }
